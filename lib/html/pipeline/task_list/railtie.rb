84660fe0a2d1fc37cd77169b8907f24ed4129c26
Andrew Conrad <andrew@versyy.com>
2020-03-26 18:24:23 -0500

    issue-4: Added Class hierarchy

--- lib/html/pipeline/task_list/railtie.rb
+++ lib/html/pipeline/task_list/railtie.rb
@@ -1,19 +1,23 @@
-class HTML::Pipeline::TaskList
-  class << self
-    def root_path
-      @root_path ||= Pathname.new(File.expand_path('../..', __dir__))
-    end
+module HTML
+  class Pipeline
+    class TaskList
+      class << self
+        def root_path
+          @root_path ||= Pathname.new(File.expand_path('../..', __dir__))
+        end
 
-    def asset_paths
-      @asset_paths ||= Dir[root_path.join('app/assets/*')]
-    end
-  end
+        def asset_paths
+          @asset_paths ||= Dir[root_path.join('app/assets/*')]
+        end
+      end
 
-  if defined? ::Rails::Railtie
-    class Railtie < ::Rails::Railtie
-      initializer 'task_list' do |app|
-        TaskList.asset_paths.each do |path|
-          app.config.assets.paths << path
+      if defined? ::Rails::Railtie
+        class Railtie < ::Rails::Railtie
+          initializer 'task_list' do |app|
+            TaskList.asset_paths.each do |path|
+              app.config.assets.paths << path
+            end
+          end
         end
       end
     end
--- lib/html/pipeline/task_list/summary.rb
+++ lib/html/pipeline/task_list/summary.rb
@@ -1,28 +1,32 @@
 require 'html/pipeline'
 
-class HTML::Pipeline::TaskList
-  # Provides a summary of provided TaskList `items`.
-  #
-  # `items` is an Array of TaskList::Item objects.
-  Summary = Struct.new(:items) do
-    # Public: returns true if there are any TaskList::Item objects.
-    def items?
-      item_count.positive?
-    end
+module HTML
+  class Pipeline
+    class TaskList
+      # Provides a summary of provided TaskList `items`.
+      #
+      # `items` is an Array of TaskList::Item objects.
+      Summary = Struct.new(:items) do
+        # Public: returns true if there are any TaskList::Item objects.
+        def items?
+          item_count.positive?
+        end
 
-    # Public: returns the number of TaskList::Item objects.
-    def item_count
-      items.size
-    end
+        # Public: returns the number of TaskList::Item objects.
+        def item_count
+          items.size
+        end
 
-    # Public: returns the number of complete TaskList::Item objects.
-    def complete_count
-      items.select(&:complete?).size
-    end
+        # Public: returns the number of complete TaskList::Item objects.
+        def complete_count
+          items.select(&:complete?).size
+        end
 
-    # Public: returns the number of incomplete TaskList::Item objects.
-    def incomplete_count
-      items.reject(&:complete?).size
+        # Public: returns the number of incomplete TaskList::Item objects.
+        def incomplete_count
+          items.reject(&:complete?).size
+        end
+      end
     end
   end
 end
